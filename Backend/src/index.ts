26d88e4614038418302dec1c74dd857c744dce64
VanshRana-1004 <ranavansh611@gmail.com>
2025-01-18 17:56:06 +0530

    fix

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -16,26 +16,10 @@ import { Request, Response } from 'express';
 const JWT_Secret=process.env.JWT_Secret || 'random';
 const mongodbUrl=process.env.mongodbUrl || 'random';
 const PORT = process.env.PORT;
-
 const app=express();
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    next();
-  });
 import cors from "cors";
-const corsOptions = {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-  };
-  app.use(cors(corsOptions));
-
+app.use(cors());
 
 const requireInput=z.object({
     username:z.string().min(1),
