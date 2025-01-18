e68a634fc355ec4d588e0080bdba02c007d57f07
VanshRana-1004 <ranavansh611@gmail.com>
2025-01-18 17:08:04 +0530

    fixed

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -33,7 +33,6 @@ const corsOptions = {
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
   };
   app.use(cors(corsOptions));
 
