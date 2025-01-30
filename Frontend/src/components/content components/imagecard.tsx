4b5a8135d937fb86cda4a45a67f64c46fc01b92f
VanshRana-1004 <ranavansh611@gmail.com>
2025-01-30 13:45:24 +0530

    fix

--- Frontend/src/components/content components/imagecard.tsx
+++ Frontend/src/components/content components/imagecard.tsx
@@ -8,9 +8,11 @@ interface params2{
 
 export function ImageCard(props : params2){
     const [element,setElement]=useState<React.ReactNode>(
-        <img src="https://cdn.dribbble.com/userupload/14704018/file/original-e2278ac50bd1c945c062e0554e4b733d.png?crop=0x0-6000x4500&format=webp&resize=450x338&vertical=center" alt="default" className="h-full w-full"/>
+        <img src="https://i.sstatic.net/xSMT3.png" alt="default" className="h-full w-full"/>
     )
-
+    const handleError = () => {
+        setElement(<img src="https://cdn.dribbble.com/userupload/14704018/file/original-e2278ac50bd1c945c062e0554e4b733d.png?crop=0x0-6000x4500&format=webp&resize=450x338&vertical=center" alt="default" className="h-full w-full"/>)
+    };
     useEffect(()=>{
         const style=`h-full w-full overflow-hidden`;
         const imageExtensions = /\.(jpg|jpeg|png|gif|bmp|webp|svg|tiff|tif|ico|avif|heif|heic|jpx|jp2)$/i;
@@ -33,28 +35,27 @@ export function ImageCard(props : params2){
                 </div>)
             }
             else if(imageExtensions.test(props.link) || props.link.startsWith("data:image/jpeg;base64,")){
-                setElement(<img src={props.link} alt="default" className={style}/>)
+                setElement(<img src={props.link} alt="default" className={style} onError={handleError}/>)
             }
             else if(props.link.startsWith("https://www.google.com/")){
-                setElement(<img src={"https://images2.thanhnien.vn/thumb_w/640/528068263637045248/2024/8/23/wired-uk-google-watching-1724377409027-17243774094672016963639.jpg"} className={style}/>)
+                setElement(<img src={"https://images2.thanhnien.vn/thumb_w/640/528068263637045248/2024/8/23/wired-uk-google-watching-1724377409027-17243774094672016963639.jpg"} className={style} onError={handleError}/>)
             }
             else if(props.link.startsWith("https://www.notion.so/")){
                 setElement(<img src={"https://i0.wp.com/iamsteve.in/wp-content/uploads/2020/11/notion-logo.png?ssl=1"} className={style}/>)
             }
             else if(props.link.startsWith("https://") || props.link.startsWith("https://www.linkedin.com/")){
                 try{
-                    console.log(`//image.thum.io/get/${props.link}`)
-                    setElement(<img src={`//image.thum.io/get/${props.link}`} className={`${style}`}/>)
+                    setElement(<img src={`//image.thum.io/get/${props.link}`} className={`${style}`} onError={handleError}/>)
                     if(props.link=='https://secondbrain-app.vercel.app/'){
-                        setElement(<img src={'landing.png'} className={`${style}  w-full h-full`}/>)
+                        setElement(<img src={'landing.png'} className={`${style}  w-full h-full`} onError={handleError}/>)
                     }
                 }
                 catch(e){
-                    setElement(<img src="https://cdn.dribbble.com/userupload/14704018/file/original-e2278ac50bd1c945c062e0554e4b733d.png?crop=0x0-6000x4500&format=webp&resize=450x338&vertical=center" alt="default" className={style}/>)
+                    setElement(<img src="https://cdn.dribbble.com/userupload/14704018/file/original-e2278ac50bd1c945c062e0554e4b733d.png?crop=0x0-6000x4500&format=webp&resize=450x338&vertical=center" alt="default" className={style} onError={handleError}/>)
                 }
             }
             else{
-                setElement(<img src="https://cdn.dribbble.com/userupload/14704018/file/original-e2278ac50bd1c945c062e0554e4b733d.png?crop=0x0-6000x4500&format=webp&resize=450x338&vertical=center" alt="default" className={style}/>)
+                setElement(<img src="https://cdn.dribbble.com/userupload/14704018/file/original-e2278ac50bd1c945c062e0554e4b733d.png?crop=0x0-6000x4500&format=webp&resize=450x338&vertical=center" alt="default" className={style} onError={handleError}/>)
             }
         }
         catch (error) {
