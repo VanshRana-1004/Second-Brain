f28574ec12a11c203fe98664a84094d7598fe657
Vansh Rana <130490295+VanshRana-1004@users.noreply.github.com>
2025-02-03 19:14:32 +0530

    Update tag.tsx

--- Frontend/src/components/content components/tag.tsx
+++ Frontend/src/components/content components/tag.tsx
@@ -8,5 +8,5 @@ interface TagProps {
 export function Tag(props : TagProps){
     let cursor='';
     if(props.hasOwnProperty('cursor')) cursor='cursor-pointer';
-    return <div className={` h-7 border border-gray-200 rounded-2xl flex flex-wrap gap-1 items-center px-2 ${props.tagRef?'text-black bg-white':'bg-black text-white'} font-custom font-regular ${cursor}`} onClick={props.onClick}><TagIcon len={"14px"} color={props.tagRef} />{props.name}</div>
+    return <div className={` h-8 tracking-wide border border-gray-200 rounded-2xl text-lg flex flex-wrap gap-1 items-center px-2 ${props.tagRef?'text-black bg-white':'bg-black text-white'} font-custom font-regular ${cursor}`} onClick={props.onClick}><TagIcon len={"14px"} color={props.tagRef} />{props.name}</div>
 }
