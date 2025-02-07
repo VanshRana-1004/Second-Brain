9fcacaf766eb8dca5e05093d685a69c4277b18ef
Vansh Rana <130490295+VanshRana-1004@users.noreply.github.com>
2025-02-08 00:24:44 +0530

    Update titlelogo.tsx

--- Frontend/src/components/sidebar components/titlelogo.tsx
+++ Frontend/src/components/sidebar components/titlelogo.tsx
@@ -5,7 +5,8 @@ interface params{
 }
 export function TitleLogo(props : params){
     return <div className={`justify-center bg-black h-16 w-fit flex px-3 ${props.size<768?'px-0':''} ease-in-out gap-5 py-2 items-center`}>
-        {props.size>=768 && <Brainicon/>}
+{/*         {props.size>=768 && <Brainicon/>} */}
+        <img src='https://raw.githubusercontent.com/luyu0279/BrainyAi/main/misc/logo.png'></img>
         <Title color={"white"} size={props.size}/>
     </div>
 }
