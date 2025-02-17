4303800792385473b3ae457833045bc6c0187cf6
Vansh Rana <130490295+VanshRana-1004@users.noreply.github.com>
2025-02-17 21:25:04 +0530

    Update sidebar.tsx

--- Frontend/src/components/sidebar components/sidebar.tsx
+++ Frontend/src/components/sidebar components/sidebar.tsx
@@ -22,7 +22,7 @@ export function Sidebar(props : PropsType){
         if(!icon) style='w-80';
         else style=`w-16`;
     }
-    return <div className={`z-50 overflow-visible top-0 left-0 flex  flex-col items-center gap-2 duration-200 ease-in-out min-h-screen border-r-2 border-r-gray-700 shadow-gray-500 shadow-md ${props.showInput?'opacity-25':'opacity-100'} ${style}  bg-black`}>
+    return <div className={`z-50 overflow-visible top-0 left-0 flex  flex-col items-center gap-2 duration-200 ease-in-out min-h-screen border-r-2 border-r-gray-700  ${props.showInput?'opacity-25':'opacity-100'} ${style}  bg-black`}>
         <AllOptions setContentType={props.setContentType} showSideBar={props.showSideBar} icon={icon} setIcon={setIcon} showMenu={props.showMenu} setShowMenu={props.setShowMenu} size={props.size}/>
     </div>
 }
